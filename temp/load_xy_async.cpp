15fc796b9430d695e3835ddd43a3672cd6f896f0
pengsun <pengsun000@gmail.com>
2015-04-26 20:48:37 -0400

    modify

--- temp/load_xy_async.cpp
+++ temp/load_xy_async.cpp
@@ -4,74 +4,70 @@
 
 using namespace std;
 
+
+static thread worker;
+static mxArray *X = 0;
+static mxArray *Y = 0;
+
+
 void read_X_Y (const char *fn);
 
-struct mat_loader {
-  thread worker;  
-  mxArray *X, *Y;
-
-  mat_loader () {
-    X = mxCreateDoubleMatrix(0,0,mxREAL);
-    Y = mxCreateDoubleMatrix(0,0,mxREAL);
+void load_mat (const char * fn) 
+{
+  if (worker.joinable()) { // wait until last loading finishes...
+    worker.join(); 
+    mexPrintf("wait until last reading done\n");
   }
 
-  void load (const char * fn) {
-    if (worker.joinable()) { // wait until last loading finishes...
-      worker.join(); 
-      mexPrintf("wait until last reading done\n");
-    }
+  // begin a new thread to load the variables
+  thread t(read_X_Y, fn);
 
-    // begin a new thread to load the variables
-    thread t(read_X_Y,  fn);
+  // return immediately
+  worker = move(t);
+}
 
-    // return immediately
-    this->worker = move(t);
-  }
+void pop_buf (mxArray* &xx, mxArray* &yy) {
+  if (worker.joinable()) 
+    worker.join();
 
-  void pop_buf (mxArray* &X, mxArray* &Y) {
-    if (worker.joinable()) 
-      worker.join();
+  // pop them: do nothing but return them to Matalb who takes the control
+  xx = X;
+  yy = Y;
+}
 
-    // pop them: do nothing but return them to Matalb who takes the control
-    X = this->X;
-    Y = this->Y;
-  }
+void clear_buf () 
+{
+  mexPrintf("destroy buffer X, Y\n");
+  mxDestroyArray(X);
+  mxDestroyArray(Y);
+}
 
-  void clear_buf () {
-    mexPrintf("destroy buffer X, Y\n");
-    mxDestroyArray(X);
-    mxDestroyArray(Y);
-  }
-
-};
-
-static mat_loader the_loader;
 
 void read_X_Y (const char *fn) {
   // TODO: need a lock here?
 
   // clean the buffer
-  the_loader.clear_buf();
+  clear_buf();
 
   mexPrintf("open mat\n");
   MATFile *h = matOpen(fn, "r");
 
   mexPrintf("read X, Y\n");
-  the_loader.X = matGetVariable(h, "X"); // TODO: check 
-  the_loader.Y = matGetVariable(h, "Y");
+  X = matGetVariable(h, "X"); // TODO: check 
+  Y = matGetVariable(h, "Y");
 
   mexPrintf("close mat\n");
   matClose(h);
 
   mexPrintf("make persistence buffer X, Y\n");
-  mexMakeArrayPersistent(the_loader.X);
-  mexMakeArrayPersistent(the_loader.Y);
+  mexMakeArrayPersistent(X);
+  mexMakeArrayPersistent(Y);
 }
 
 void on_exit ()
 {
-  the_loader.clear_buf();
-  the_loader.worker.~thread();
+  clear_buf();
+  worker.~thread();
 }
 
 
@@ -84,18 +80,18 @@ void mexFunction(int no, mxArray       *vo[],
   if (ni==1) {
     // get the file name 
     int buflen = mxGetN(vi[0])*sizeof(mxChar)+1;
-    char *buf  = (char*)mxMalloc(buflen);
-    mxGetString(vi[0], buf, buflen); // TODO: check status
+    char *bufFN  = (char*)mxMalloc(buflen);
+    mxGetString(vi[0], bufFN, buflen); // TODO: check status
 
     // begin loading and return 
-    the_loader.load(buf);
+    load_mat(buf);
     mexAtExit( on_exit );
     return;
   }
 
   // [X,Y] = load_xy_async():  loads the X, Y from buffer
   if (ni==0) {
-    the_loader.pop_buf(vo[0], vo[1]);
+    pop_buf(vo[0], vo[1]);
     return;
   }
 
