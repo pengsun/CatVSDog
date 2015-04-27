549f500ad03e0d4ddd605fe1df37113d581ad657
peng sun <pengsun000@gmail.com>
2015-04-27 11:25:51 -0400

    m

--- temp/make.m
+++ temp/make.m
@@ -9,9 +9,11 @@ switch str(1:3)
   case 'win' 
     opt{end+1} = 'COMPFLAGS=/openmp $COMPFLAGS';
     opt{end+1} = 'LINKFLAGS=/openmp $LINKFLAGS';
+  case 'gln'
+    opt{end+1} = 'CXXFLAGS="\$CXXFLAGS -pthread -std=c++11"';
+    opt{end+1} = 'LDFLAGS="\$LDFLAGS -pthread -std=c++11"';
   otherwise
-    opt{end+1} = 'CFLAGS=''$CFLAGS -pthread -std=c++11''';
-    opt{end+1} = 'LDFLAGS=''$LDFLAGS -pthread -std=c++11''';
+    error('unsupported platform');
 end
 %% do it
 mex(opt{:}, 'load_xy_async.cpp');
